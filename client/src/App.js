5db219d85830c668b312085237494b9e706a70b9
kirtanchandak <themarketingkirtan@gmail.com>
2023-03-19 15:49:16 +0530

    responsive navbar done

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,18 @@ import Home from "./pages/Home";
 import Auth from "./pages/Auth";
 import CreateRecipie from "./pages/CreateRecipie";
 import SavedRecipe from "./pages/SavedRecipe";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/create-recipe" element={<CreateRecipie />} />
-          <Route path="/" element={<SavedRecipe />} />
+          <Route path="/saved-recipe" element={<SavedRecipe />} />
         </Routes>
       </Router>
     </div>
