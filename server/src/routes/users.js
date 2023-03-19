4d30c7b9781d14674c1a29db72a8fb18c107b312
kirtanchandak <themarketingkirtan@gmail.com>
2023-03-19 15:10:02 +0530

    login API endpoint

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -20,6 +20,22 @@ router.post("/register", async (req, res) => {
   res.json({ message: "User added successfully" });
 });
 
-router.post("/login");
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await UserModel.findOne({ username });
+
+  if (!user) {
+    return res.json({ message: "Please regiter first and then login" });
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordCorrect) {
+    return res.json({ message: "Username or password is incorrect" });
+  }
+
+  const token = jwt.sign({ id: user._id }, "secret");
+  res.json({ token, userID: user._id });
+});
 
 export { router as userRouter };
