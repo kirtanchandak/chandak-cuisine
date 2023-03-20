07c7800cddc911533b98c5d7f444202504224397
kirtanchandak <themarketingkirtan@gmail.com>
2023-03-20 19:22:16 +0530

    save and display saved recipe API endpoints

--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import { RecipeModel } from "../models/Recipies.js";
+import { UserModel } from "../models/Users.js";
 
 const router = express.Router();
 
@@ -23,11 +24,26 @@ router.post("/", async (req, res) => {
   }
 });
 
+// save a recipe
 router.put("/", async (req, res) => {
-  const newRecipe = new RecipeModel(req.body);
   try {
-    const response = await newRecipe.save();
-    res.json(response);
+    const recipe = await RecipeModel.findById(req.body.recipeID);
+    const user = await UserModel.findById(req.body.userID);
+    user.savedRecipes.push(recipe);
+    res.json({ savedRecipes: user.savedRecipes });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//display saved recipe by a user
+router.get("/savedrecipe", async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.body.userID);
+    const savedRecipes = await RecipeModel.find({
+      _id: { $in: user.savedRecipes },
+    });
+    res.json({ savedRecipes });
   } catch (err) {
     console.log(err);
   }
